d198b99b713bfb6bef5c7efde5294bac6319de11
mariaasenova <mariaasenova22@gmail.com>
2017-12-14 13:16:28 +0100

    changes on paragraphs

--- examcase/js/viko.js
+++ examcase/js/viko.js
@@ -1,7 +1,16 @@
 // JavaScript Document
-$("#burger").on("touch",funtion(){
+// The jQuery's .ready method checks if the page is ready for your code to work with
+$(document). ready(function () {
+
+$("#burger").on("touch",funtion {
 				$("#myDropdown").show();
-				});
+	
+	});
+	
+	
+});
 				
 				
-			
+//$("#burger").click( function(){
+    //$('#myDropdown').hide();
+				//});			

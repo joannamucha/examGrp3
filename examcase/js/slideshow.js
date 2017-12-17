01fe28e2f5834e6763d20cd3bfd80c8ef70b9dd7
mariaasenova <mariaasenova22@gmail.com>
2017-12-17 15:11:36 +0100

    pseudo code for js
    scheck lideshow and on click documents

--- examcase/js/slideshow.js
+++ examcase/js/slideshow.js
@@ -1,8 +1,12 @@
 // JavaScript Document
-    
+//Source: www.w3schools.com/w3css/w3css_slideshow.asp    
+
+
+//set the slideIndex to 1. (First picture)
 var slideIndex = 1;
 showDivs(slideIndex);
-
+//Then call showDivs() to display the first image
+//The plusDivs() function subtracts one or  adds one to the slideIndex
 function plusDivs(n) {
     showDivs(slideIndex += n);
 }
@@ -10,9 +14,12 @@ function plusDivs(n) {
 function showDivs(n) {
     var i;
     var x = document.getElementsByClassName("myslidesstories");
+	//If the slideIndex is less than 1 it is set to number of elements (x.length)
     if (n > x.length) {slideIndex = 1}
     if (n < 1) {slideIndex = x.length} ;
     for (i = 0; i < x.length; i++) {
+		//The showDiv() function hides (display="none") all elements with the class name "mySlides", and displays (display="block") the element with the given slideIndex
+		//If the slideIndex is higher than the number of elements (x.length), the slideIndex is set to zero
         x[i].style.display = "none";
     }
     x[slideIndex-1].style.display = "block"; 
--- examcase/js/viko.js
+++ examcase/js/viko.js
@@ -1,11 +1,11 @@
 // JavaScript Document
 // The jQuery's .ready method checks if the page is ready for your code to work with
 $(document). ready(function () {
-
+// here we that if the button with #burger in on touch ul should display 
 $("#burger").on("touch",funtion {
 				$("#myDropdown").show();
 	
-	});
+
 	
 	
 });

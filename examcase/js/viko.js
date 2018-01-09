67457b17da343cb1613cbe4afa257713960daca1
mariaasenova <mariaasenova22@gmail.com>
2018-01-09 17:02:40 +0100

    click event
    it still does not work

--- examcase/js/viko.js
+++ examcase/js/viko.js
@@ -1,14 +1,22 @@
-// JavaScript Document
-// The jQuery's .ready method checks if the page is ready for your code to work with
-$(document). ready(function () {
-// here we that if the button with #burger in on touch ul should display 
-$("#burger").on("touch",funtion {
-				$("#myDropdown").show();
-	
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction('.dropbtn') {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
 
-	
-	
-});
-				
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}		
 				
 			
